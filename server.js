d9f075f2a68f32295b49d5d412cd904fe6f4dfdb
prototriangle <prototriangle@gmail.com>
2018-02-12 02:50:32 +0000

    Implement more methods, remove PATCH, convert lookup functions to pass on index

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ storeIfNotStored("lastReqId", 0);
 storeIfNotStored("inventory", []);
 storeIfNotStored("lastInvId", 0);
 storeIfNotStored("robots", []);
+storeIfNotStored("lastRobId", 0);
 
 app.use(bodyParser.json());
 
@@ -37,18 +38,37 @@ var statusRouter = express.Router();
 var inventoryRouter = express.Router();
 
 function lookupRobot(req, res, next) {
-  var robotId = req.body.id;
-  robotsFiltered = storage.getItemSync("robots").robots.filter(robot => robot.id == robotId);
-  if (robotsFiltered.length == 0) {
+  const robotIndex = storage.getItemSync("robots").findIndex(robot => robot.id == req.params.id;
+  if (robotIndex == -1) {
     res.statusCode = 404;
     return res.json({errors: ["Robot not found"]});
   }
-  req.robot = robotsFiltered[0];
+  req.robotIndex = robotIndex;
+  next();
+}
+
+function lookupItem(req, res, next) {
+  const itemIndex = storage.getItemSync("inventory").findIndex(item => item.code == req.params.id);
+  if (itemIndex == -1) {
+    res.statusCode = 404;
+    return res.json({errors: ["Item not found"]});
+  }
+  req.itemIndex = itemIndex;
   next();
 }
 
 statusRouter.get('/', function(req, res) {});
-statusRouter.post('/', function(req, res) {});
+statusRouter.post('/', function(req, res) {
+  var id = mutate("lastInvId", val => val + 1);
+  var item = req.body;
+  item.id = id;
+  var inventory = mutate("inventory", val => {
+    val.push(request);
+    return val;
+  });
+  console.log(inventory);
+  res.send(item);
+});
 statusRouter.get('/:id', lookupRobot, function(req, res) {});
 statusRouter.patch('/:id', lookupRobot, function(req, res) {});
 statusRouter.delete('/:id', lookupRobot, function(req, res) {});
@@ -73,9 +93,25 @@ inventoryRouter.post('/', function(req, res) {
   console.log(inventory);
   res.send(item);
 });
-inventoryRouter.get('/:id', lookupRobot, function(req, res) {});
-inventoryRouter.patch('/:id', lookupRobot, function(req, res) {});
-inventoryRouter.delete('/:id', lookupRobot, function(req, res) {});
+inventoryRouter.get('/:id', lookupItem, function(req, res) {
+  res.send(storage.getItemSync("inventory")[req.itemIndex]);
+});
+inventoryRouter.put('/:id', lookupItem, function(req, res) {
+  var item = req.body;
+  item.id = req.params.id;
+  var inventory = mutate("inventory", val => {
+    val[req.itemIndex] = item;
+    return val;
+  });
+  res.send(item)
+});
+inventoryRouter.delete('/:id', lookupItem, function(req, res) {
+  mutate("inventory", val => {
+    val.splice(req.itemIndex, 1);
+    return val;
+  });
+  res.statusCode = 204;
+});
 app.use('/api/inventory', inventoryRouter);
 
 app.get('/api/requests/', (req, res) => {
