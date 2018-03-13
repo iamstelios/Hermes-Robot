4cbf3b3f4a3dfb53420ddd0e71324df4ec8ab225
Abbi <abbiculemann@gmail.com>
2018-03-13 18:13:32 +0000

    linefollower script being formed

---
+++ robot/linefollower.py
@@ -0,0 +1,100 @@
+from time import sleep
+from ev3dev.auto import *
+
+mLeft = LargeMotor('outD')
+mRight = LargeMotor('outC')
+cLeft = ColorSensor('in3')
+cRight = ColorSensor('in4')
+
+mRight.polarity = 'inversed'
+mLeft.polarity = 'inversed'
+
+mPower = 75
+
+# min and max reflection
+minRng = 1
+maxRng = 88
+
+#
+trg = 55
+
+# PID variables
+kp = float(0.6)
+ki = float(0.05)
+kd = 0.3
+
+# For black line white background:
+# -1 if sensor on left side, 1 if on right side
+
+# 0: No color
+# 1: Black
+# 2: Blue
+# 3: Green
+# 4: Yellow
+# 5: Red
+# 6: White
+# 7: Brown
+
+direction = 1
+junctionmarker = 1
+
+
+def steering(course, power):
+    power_left = power_right = power
+    s = (50 - abs(float(course))) / 50
+    if course >= 0:
+        power_right *= s
+        if course > 100:
+            power_right = - power
+    else:
+        power_left *= s
+        if course < -100:
+            power_left = - power
+    return int(power_left), int(power_right)
+
+
+def pid(mPower, trg, kp, kd, ki, direction, minRng, maxRng, coloursens, linesens, stopcolour):
+    linesens.mode = 'COL-REFLECT'
+    coloursens.mode = 'COL-COLOR'
+    lastError = error = integral = 0
+    mLeft.run_direct()
+    mRight.run_direct()
+
+    while coloursens.color != stopcolour:
+        print(coloursens.color)
+        refRead = linesens.value()
+
+        error = trg - (100 * (refRead - minRng) / (maxRng - minRng))
+        derivative = error - lastError
+        lastError = error
+        integral = float(0.5) * integral + error
+        course = (kp * error + kd * derivative + ki * integral) * direction
+
+        for (motor, pow) in zip((mLeft, mRight), steering(course, mPower)):
+            motor.duty_cycle_sp = pow
+        sleep(0.01)
+
+
+def hook(exitcolour):
+    rounddirection = -direction
+    # move right to catch line
+    mRight.stop()
+    mLeft.stop()
+    time = 500
+    mLeft.run_timed(time_sp=time, speed_sp=100)
+    mRight.run_timed(time_sp=time, speed_sp=250)
+    sleep(time / 1000)
+    # pid on inside
+    pid(mPower, trg, kp, kd, ki, rounddirection, minRng, maxRng, coloursens=cRight, linesens=cLeft,
+        stopcolour=exitcolour)
+    time = 500
+    mLeft.run_timed(time_sp=time, speed_sp=250)
+    mRight.run_timed(time_sp=time, speed_sp=100)
+    sleep(time / 1000)
+    pid(mPower, trg, kp, kd, ki, minRng, maxRng, coloursens=cLeft, linesens=cRight,
+        stopcolour=junctionmarker)
+
+
+def run():
+    pid(mPower, trg, kp, kd, ki, minRng, maxRng, coloursens=cLeft, linesens=cRight,
+        stopcolour=junctionmarker)
