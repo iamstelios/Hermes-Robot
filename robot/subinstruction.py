9dabe4aba8fcc6ea64f67e8c9468ea7e9e162c18
Stelios <apollon_g1_ste@hotmail.com>
2018-03-19 16:09:09 +0000

    Add __str__ to subinstructions

--- robot/subinstruction.py
+++ robot/subinstruction.py
@@ -35,7 +35,9 @@ class Move(SubInstruction):
     # Nodes can be either workstations or junctions or the base
     # Robot will be facing the right direction when starting this
     # instruction (Node A -> Node B)
-
+    def __str__(self):
+        return 'Move(%s,%s,%s)' % (self.nodeA, self.nodeB, self.exit)
+        
     # If node B is a junction, the robot should stop before entering the juction
     # Thus if the opposite is needed no exit input is needed to reach back node A.
     def __init__(self, nodeA, nodeB, exit=None):
@@ -64,6 +66,9 @@ class Move(SubInstruction):
 
 class Reverse(SubInstruction):
     # Reverses the position of the robot 180 degrees
+    def __str__(self):
+        return 'Reverse()'
+
     def run(self):
         print('Reversing direction')
 
@@ -101,6 +106,9 @@ class Reverse(SubInstruction):
 
 class BasePickUp(SubInstruction):
     # Picks up item at shelf level
+    def __str__(self):
+        return 'BasePickUp(%s)' % (self.level)
+
     def __init__(self, level):
         self.level = level
 
@@ -140,6 +148,9 @@ class BasePickUp(SubInstruction):
 
 class BaseDrop(SubInstruction):
     # Drops item at shelf position
+    def __str__(self):
+        return 'BaseDrop(%s)' % (self.level)
+    
     def __init__(self, level):
         self.level = level
 
@@ -175,6 +186,9 @@ class BaseDrop(SubInstruction):
 
 class WorkstationPickUp(SubInstruction):
     # Takes the box from the workstation
+    def __str__(self):
+        return 'WorkstationPickUp()'
+
     def run(self):
         print('Picking up box from workstation')
 
@@ -204,6 +218,9 @@ class WorkstationPickUp(SubInstruction):
 
 class WorkstationDrop(SubInstruction):
     # Delivers the box to the workstation
+    def __str__(self):
+        return 'WorkstationDrop()'
+    
     def run(self):
         print('Dropping box to workstation')
 
