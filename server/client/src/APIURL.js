fa246f7047b718b27b765a6966b38368196ca335
prototriangle <prototriangle@gmail.com>
2018-03-23 10:30:55 +0000

    Fix port issue when using production version on localhost

--- server/client/src/APIURL.js
+++ server/client/src/APIURL.js
@@ -1,9 +1,10 @@
 console.log(window.location.hostname);
+console.log(window.location.origin);
 console.log(process.env.NODE_ENV);
 
 let apiUrl = '';
 if (process.env.NODE_ENV === 'production') {
-    apiUrl = 'http://' + window.location.hostname;
+    apiUrl = window.location.origin;
 }
 
 export default apiUrl;
